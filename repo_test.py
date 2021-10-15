7918385d88edb0aaf9fb7127b8ad9d359e580abe
Marcus Chan <marcus.chan@generalfusion.com>
2021-10-15 14:42:58 -0700

    entering the library for the github repo and the run file for the test

---
+++ repo_test.py
@@ -0,0 +1,80 @@
+from git import Repo
+import os
+
+class reg_file:
+    def __init__(self):
+        self.path_toSelf = "H:/"  # when we load this on the server this will be fixed
+        self.username = "MarcusGenFus"
+        self.access_token = "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"
+        #self.remote = f"https://{self.username}:{self.access_token}@github.com/GeneralFusion/PIStatusWebApp.git"
+        self.remote = f"https://{self.username}:{self.access_token}@https://github.com/MarcusGenFus/TestAutoRepo.git"
+        self.repo = self.get_updated_repo()
+        self.branchN = ""
+        self.changes_made = []
+        self.file = ""
+
+    # it should be possible to use the function below as a reset to any changes made in the session
+    def get_updated_repo(self):
+        return Repo.clone_from(self.remote, self.path_toSelf)
+
+    def read_last_branch(self):
+        curr_dir = os.getcwd() # this will have to be swapped with a static place later
+        try:
+            with open(curr_dir+"\\last_branch.txt", 'r') as file:
+                self.branchN = file.read()
+                file.close()
+            self.mount_branch(self.branchN)
+            return True
+        except Exception as e:
+            print("Previous version of the last branch could not be found")
+            return False
+
+    def write_last_branch(self):
+        curr_dir = os.getcwd() # this will have to be swapped with a static place later
+        with open(curr_dir + "\\last_branch.txt", 'w+') as file:
+            file.write(self.branchN)
+            file.close()
+
+    def list_branches(self):
+        branch_list = []
+        for x in self.repo.heads:
+            branch_list.append(x.name)
+        return branch_list.copy()
+
+    def mount_branch(self, branchName):
+        # this piece of code was created by Ali Esbak modified to fit this implementation
+        # Set the branch, first checking to make sure the branch exists on the remote
+        remoteBranch = [x for x in self.repo.remote().refs if x.name[7:] == branchName]
+        if len(remoteBranch) == 0:
+            print([x.name for x in self.repo.remote().refs])
+            raise RuntimeError('Remote branch {} not found for {}- make it first then rerun'.format(branchName,
+                                                                                                    self.repo.working_tree_dir))
+        # Check for local branch, if found check it out and pull to it
+        localBranch = [x for x in self.repo.heads if x.name == branchName]
+        if len(localBranch) == 1:
+            localBranch[0].checkout()
+            self.repo.git.pull()
+        else:
+            # Otherwise, create the local branch
+            branch = self.repo.create_head(branchName, remoteBranch[0])
+            branch.set_tracking_branch(remoteBranch[0]).checkout()
+            self.repo.head.reference = branch
+        print("Branch has been mounted")
+
+    def repo_file_checkout(self, filename):
+        self.repo_file_release()
+        self.file = filename
+
+    def repo_file_release(self):
+        # returns the file checked out and references it in sql table
+        self.file = ""
+        pass
+
+    def update_config(self):
+        # this should push a single file to the repo
+        # that file should be the file that they checked out
+        #self.repo.index.add(f"H:/PIStatusWebApp/WebAppRemoteServer/configs/{self.file}")
+        self.repo.index.add(f"H:/TestAutoRepo/files/{self.file}")
+        self.repo.index.commit(f"Updating {self.file}")
+        origin = self.repo.remote(name="origin")
+        origin.push()
---
+++ run.py
@@ -0,0 +1,18 @@
+from repo_test import reg_file
+import os
+
+def main():
+    re = reg_file()
+    print(re.list_branches())
+    temp = re.read_last_branch()
+    if temp == False:
+        re.mount_branch("main")
+    re.repo_file_checkout("testfile1.txt")
+    with open("H:/TestAutoRep/files/testfile1.txt", 'a') as file:
+        file.write("new message")
+        file.close()
+    re.update_config()
+    re.repo_file_release()
+
+if __name__ == "__main__":
+    main()
